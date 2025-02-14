f252830b7c7776aea54880302b44bc3b48a3d131
Aa-ra-dhana5 <aaradhanaparmar55@gmail.com>
2025-02-14 10:18:40 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Chai from './chai'
+
+function App() {
+  
+  return (
+       <>
+      
+      <Chai/>
+      <h1>Chai or react with vite | Aaradhana</h1>
+      </>
+  )
+}
+
+export default App
